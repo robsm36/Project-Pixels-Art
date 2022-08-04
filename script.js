2fafb82035e56ec329e3f758c8533c1c84465919
Robson Miranda <robsm36@gmail.com>
2022-08-04 12:36:51 -0300

    Requisito 9 - Crie um botão que retorne a cor do quadro para a cor inicial.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let pixelBoard = document.querySelector('#pixel-board');
 pixelBoard.id = 'pixel-board';
 
-for (i = 1; i < 26; i += 1) {
+var inputNum = 5;
+var totalPixels = inputNum * inputNum;
+var counter = 0;
+
+for (i = 0; i < totalPixels; i += 1) {
+    counter += 1;
     var divv = document.createElement('div');
     divv.className = 'pixel';
     divv.style.backgroundColor = 'white';
     pixelBoard.appendChild(divv);
-}
 
+    if (counter % inputNum == 0) {
+        pixelBoard.appendChild(document.createElement('br'));
+    }
+}
 
 let orangeColor = document.querySelector('.orange');
 let greenColor = document.querySelector('.green');
@@ -28,7 +36,7 @@ greenColor.addEventListener("click", selectColor);
 purpleColor.addEventListener("click", selectColor);
 blackColor.addEventListener("click", selectColor);
 
-let pixelList = document.querySelectorAll('.pixel');
+var pixelList = document.querySelectorAll('.pixel');
 
 function tintPixel (event) {
     let selectedColor = document.createElement('div');
@@ -36,11 +44,20 @@ function tintPixel (event) {
     selectedColor.classList.remove("selected");
     selectedColor.classList.remove("color");
     selectedColor.classList.add("pixel");
-    let teste = event.target;
-    teste.style.removeProperty('background-color');
-    teste.classList = selectedColor.classList;
+    let clickTarget = event.target;
+    clickTarget.style.removeProperty('background-color');
+    clickTarget.classList = selectedColor.classList;
 }
 
-for (i = 0; i < 25; i += 1) {
+for (i = 0; i < totalPixels; i += 1) {
     pixelList[i].addEventListener("click", tintPixel);
 }
+
+function clearBoard () {
+    for (let i = 0; i < pixelList.length; i += 1){
+    pixelList[i].style.backgroundColor = 'white';
+}
+}
+
+let clearButton = document.querySelector('#clear-board');
+clearButton.addEventListener("click", clearBoard)
